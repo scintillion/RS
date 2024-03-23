5d84375903812ce740e17ca241243fff2427c473
scintillion <43735913+scintillion@users.noreply.github.com>
2024-03-23 15:24:34 -0400

    Add_RS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script lang="ts">
+import { RS1 } from '$lib/RS'
+import { onMount } from 'svelte'
+
+onMount(() => {
+	console.log ('Entry InitClient!');
+	// InitClient ();
+});
+</script>
+
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Second Message!</h1>
